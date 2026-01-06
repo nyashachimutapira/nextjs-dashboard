ef1187bcff97ed93bc9bcaf204c7ab0a2f751343
nyashachimutapira <chimutapiran@gmail.com>
2026-01-06 19:05:34 +0200

    commit

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -99,7 +99,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
   redirect('/dashboard/invoices');
 }
 
-export async function deleteInvoice(id: string) {
+export async function deleteInvoice(id: string, formData: FormData): Promise<void> {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
   } catch (error) {
